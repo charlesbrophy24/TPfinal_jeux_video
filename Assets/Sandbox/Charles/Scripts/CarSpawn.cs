ea1985b742c89191c33cd9d15049e6a6b8142dfa
Brophy Charles <charlesbrophy05@gmail.com>
2024-12-15 12:12:29 -0500

    test

---
+++ Assets/Sandbox/Charles/Scripts/CarSpawn.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Meta.XR.MRUtilityKit;
+
+public class CarSpawn : MonoBehaviour
+{
+
+    public float spawnTimer = 1;
+    public GameObject prefabToSpawn;
+    private float timer;
+
+    public MRUKAnchor.SceneLabels spawnLabels;
+
+    public float minEdgeDistance = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (!MRUK.Instance && !MRUK.Instance.IsInitialized)
+        return;
+
+        timer += Time.deltaTime;
+        if (timer > spawnTimer){
+
+            SpawnGhosts();
+            timer -= spawnTimer;
+
+        }
+
+
+    }
+
+
+    public void SpawnGhosts(){
+
+        MRUKRoom room = MRUK.Instance.GetCurrentRoom();
+        
+        room.GenerateRandomPositionOnSurface(MRUK.SurfaceType.FACING_UP, minEdgeDistance, LabelFilter.Included(spawnLabels), out Vector3 pos, out Vector3 norm);
+
+        
+
+        Instantiate(prefabToSpawn);
+
+    }
+}
